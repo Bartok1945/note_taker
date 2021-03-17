c4bc2a087fd606df9dd541de0f7cbb26cb50b1b8
Bartok <thom.huenger@gmail.com>
2021-03-17 11:47:19 -0400

    added routes and stuff

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,27 @@
+const noteData = require("..db/db.json");
+const fs = require("fs");
+
+module.exports = function(app) {
+    app.get("/api/notes", (req, res) => res.json(noteData));
+    
+    app.post("/api/notes", (req, res) => {
+        const newNote = req.body;
+        const uniqueID = (notesData.length).toString();
+        newNote.id = uniqueID;
+        noteData.push(newNote);
+
+        fs.writeFileSync("./db/db.json", JSON.stringify(noteData));
+        return res.json(noteData);
+    });
+};
+
+app.delete("/api/nots/:id", (req, res) => {
+    const { id } = req.params;
+    const noteIndex = noteData.findIndex((n) => n.id == id);
+    noteData.splice(noteIndex, 1);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notesData));
+    return res.json(noteData);
+});
 // * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
 
 // * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,14 @@
+// DEPENDENCIES
+const path = require("path");
 
+module.exports = function(app) {
 //   * GET `/notes` - Should return the `notes.html` file.
+app.get("/api/notes", (req, res) => {
+    res.getNotes(path.join(__dirname, "../public/notes.html"));
+});
 
 //   * GET `*` - Should return the `index.html` file
+app.get("*", (req, res) => {
+    res.getNotes(path.join(__dirname, "../public/index.html"));
+});
+};
