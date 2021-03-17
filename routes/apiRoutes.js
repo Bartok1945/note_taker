8049b0570e50243bc42a5b7dc2144b5018dc0d64
Bartok <thom.huenger@gmail.com>
2021-03-17 13:01:21 -0400

    got the port working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -20,7 +20,7 @@ module.exports = function(app) {
         fs.writeFileSync("../db/db.json", JSON.stringify(noteData));
         return res.json(noteData);
     });
-};
+
 
 // * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 
@@ -31,4 +31,6 @@ app.delete("/api/notes/:id", (req, res) => {
     fs.writeFileSync("../db/db.json", JSON.stringify(notesData));
     return res.json(noteData);
 });
+
+};
 
